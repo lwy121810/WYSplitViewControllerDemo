b63bee30a79a1b9f11cb6bbd7da7c54f099b77e8
lwy121810 <lwy1218@192.168.2.10>
2016-09-19 14:45:38 +0800

    Initial Commit

---
+++ WYSplitViewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYSplitViewControllerDemo
+//
+//  Created by lwy1218 on 16/9/19.
+//  Copyright © 2016年 lwy1218. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WYSplitViewControllerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WYSplitViewControllerDemo
+//
+//  Created by lwy1218 on 16/9/19.
+//  Copyright © 2016年 lwy1218. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
